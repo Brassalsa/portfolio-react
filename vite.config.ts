abcfcf4084248b07b458dab2d89cbef7a2a78335
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-11 19:18:08 +0530

    fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgrLoader from "@svgr/rollup";
-import { ghPages } from "vite-plugin-gh-pages";
+// import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrLoader(), ghPages()],
+  plugins: [react(), svgrLoader()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
