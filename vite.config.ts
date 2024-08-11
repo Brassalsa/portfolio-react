4934da77b7e9fc4d99632cd54b02c24295ccc333
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-11 18:32:12 +0530

    add gh-pages plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgrLoader from "@svgr/rollup";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrLoader()],
+  base: "/",
+  plugins: [react(), svgrLoader(), ghPages()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
